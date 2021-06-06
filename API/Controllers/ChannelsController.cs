a7d0f55ef1ca74abd8c3ee18390e3e0378f2849f
Francisco <javiermartincuenca@gmail.com>
2021-06-06 10:42:24 +0200

    Firsts endpoints using MediaR

--- API/Controllers/ChannelsController.cs
+++ API/Controllers/ChannelsController.cs
@@ -2,6 +2,9 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Application.Channels;
+using Domain;
+using MediatR;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Logging;
@@ -13,25 +16,23 @@ namespace API.Controllers
   [Route("api/[controller]")]
   public class ChannelsController : ControllerBase
   {
-    private readonly DataContext _context;
-    private readonly ILogger<ChannelsController> _logger;
+    private readonly IMediator _mediator;
 
-    public ChannelsController(DataContext context, ILogger<ChannelsController> logger)
+    public ChannelsController(IMediator mediator)
     {
-      _context = context ?? throw new ArgumentNullException(nameof(context));
-      _logger = logger ?? throw new ArgumentNullException(nameof(logger));
+      _mediator = mediator ?? throw new ArgumentNullException(nameof(mediator));
     }
 
     [HttpGet]
-    public async Task<IActionResult> GetChannels()
+    public async Task<ActionResult<List<Channel>>> GetChannels()
     {
-      return Ok(await _context.Channels.ToListAsync());
+      return await _mediator.Send(new List.Query());
     }
 
     [HttpGet("{id}")]
-    public async Task<IActionResult> GetChannel(Guid id)
+    public async Task<ActionResult<Channel>> GetChannel(Guid id)
     {
-      return Ok(await _context.Channels.FirstOrDefaultAsync(x => x.Id == id));
+      return await _mediator.Send(new Details.Query { Id = id });
     }
   }
 }
