2152cddb761f271a855457318483afe632cfaf51
Francisco <javiermartincuenca@gmail.com>
2021-06-29 21:30:12 +0200

    Markup Register form

--- client-app/src/components/Auth/Register.tsx
+++ client-app/src/components/Auth/Register.tsx
@@ -1,11 +1,56 @@
-import React from 'react'
+import React from 'react';
+import { Button, Form, Grid, Header, Icon, Segment } from 'semantic-ui-react';
 
-const Register:React.FC = () => {
+const Register: React.FC = () => {
   return (
-    <div>
-      <h1>Register</h1>
-    </div>
-  )
-}
+    <Grid textAlign='center' verticalAlign='middle' className='app'>
+      <Grid.Column style={{ maxWidth: 450 }}>
+        <Header as='h1' icon color='orange' textAlign='center'>
+          <Icon name='puzzle piece' color='orange' />
+          Register for NetChat
+        </Header>
+        <Form size='large'>
+          <Segment stacked>
+            <Form.Input
+              fluid
+              name='username'
+              icon='user'
+              iconPosition='left'
+              placeholder='Username'
+              type='text'
+            />
+            <Form.Input
+              fluid
+              name='email'
+              icon='mail'
+              iconPosition='left'
+              placeholder='Email address'
+              type='email'
+            />
+            <Form.Input
+              fluid
+              name='password'
+              icon='lock'
+              iconPosition='left'
+              placeholder='Password'
+              type='password'
+            />
+            <Form.Input
+              fluid
+              name='confirmPassword'
+              icon='lock'
+              iconPosition='left'
+              placeholder='Confirm password'
+              type='password'
+            />
+            <Button color='orange' fluid size='large'>
+              Register
+            </Button>
+          </Segment>
+        </Form>
+      </Grid.Column>
+    </Grid>
+  );
+};
 
-export default Register
+export default Register;
