5427292928173b5669f4b950539ade4d1442265a
Francisco <javiermartincuenca@gmail.com>
2021-07-04 19:57:11 +0200

    Add patch to solve the problem with registration

--- API/Controllers/UserController.cs
+++ API/Controllers/UserController.cs
@@ -14,6 +14,7 @@ namespace API.Controllers
       return await Mediator.Send(query);
     }
 
+    // TODO: Uncomment when add DTO
     // [HttpPost("register")]
     // public async Task<ActionResult<User>> Register(Register.Command command)
     // {
